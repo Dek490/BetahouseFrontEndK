32949e0e0c36b68e80dfc51cea25e64fa6b66eff
Kaynaanyare11 <khudari.januune.xassan@gmail.com>
2023-08-01 03:47:31 -1200

    React Quill

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,27 @@
-import React from 'react'
-
+import { Button, Stack } from '@mui/material';
+import React, { useState } from 'react';
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
 export default function About() {
+    const [fewdescription, setFewdescription] = useState('');
+    const [moredescription, setMoredescription] = useState('');
+
+    const Update =()=>{
+        console.log(fewdescription,moredescription)
+        
+
+
+    }
+
+    
   return (
-    <div>About</div>
+    <>
+   <Button sx={{alignContent:'center',bgcolor:'primary.dark',width:'10%',marginLeft:'40%'}} onClick={Update}>Update</Button>
+    <Stack direction={'row'} spacing={2}>
+    <ReactQuill theme="snow" value={fewdescription} onChange={setFewdescription} />
+    <ReactQuill theme="snow" value={moredescription} onChange={setMoredescription} />
+     
+    </Stack>
+    </>
   )
 }
